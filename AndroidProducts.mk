9ae1614a5228d2ed986e0830c48f8e7eda14f985
haotian2004 <3210467831@qq.com>
2022-09-06 12:31:58 +0800

    first commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_pufferx5.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_pufferx5-user \
+    omni_pufferx5-userdebug \
+    omni_pufferx5-eng
